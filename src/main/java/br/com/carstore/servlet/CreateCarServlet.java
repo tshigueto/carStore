3e32540e44d6c1a9aadcc89f61b8a9b620a0a7df
THIAGO SHIGUETO HOSSAKA <thiago.shossaka@senacsp.edu.br>
2023-09-29 21:26:31 -0300

    Criando o método para INSERT e configurando a conexão com o com o banco de dados

--- src/main/java/br/com/carstore/servlet/CreateCarServlet.java
+++ src/main/java/br/com/carstore/servlet/CreateCarServlet.java
@@ -1,5 +1,8 @@
 package br.com.carstore.servlet;
 
+import br.com.carstore.dao.CarDao;
+import br.com.carstore.model.Car;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -16,6 +19,10 @@ public class CreateCarServlet extends HttpServlet {
         String carName = request.getParameter("car-name");
 
         System.out.println(carName);
+        CarDao dao = new CarDao();
+        Car car = new Car();
+        car.setName(carName);
+        dao.createCar(car);
 
         request.getRequestDispatcher("index.html").forward(request, response);
 
