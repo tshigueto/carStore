1925ca29d5db29791a5456e3456a09a3ebf5632c
THIAGO SHIGUETO HOSSAKA <thiago.shossaka@senacsp.edu.br>
2023-10-06 21:27:11 -0300

    lab 3

--- src/main/java/br/com/carstore/model/Car.java
+++ src/main/java/br/com/carstore/model/Car.java
@@ -1,8 +1,19 @@
 package br.com.carstore.model;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class Car {
 
     private String name;
+
+    public Car(String carName) {
+    }
 
     public String getName() {
         return name;
@@ -10,6 +21,48 @@ public class Car {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public List<Car> findAllCars() {
+
+        String SQL = "SELECT * FROM CAR";
+
+        try {
+
+            Connection connection = DriverManager.getConnection("jdbc:h2:~/test", "sa", "sa");
+
+            System.out.println("success in database connection");
+
+            PreparedStatement preparedStatement = connection.prepareStatement(SQL);
+
+            ResultSet resultSet = preparedStatement.executeQuery();
+
+            List<Car> cars = new ArrayList<>();
+
+            while (resultSet.next()) {
+
+                String carName = resultSet.getString("name");
+
+                Car car = new Car(carName);
+
+                cars.add(car);
+
+            }
+
+            System.out.println("success in select * car");
+
+            connection.close();
+
+            return cars;
+
+        } catch (Exception e) {
+
+            System.out.println("fail in database connection");
+
+            return Collections.emptyList();
+
+        }
+
     }
 
 }
